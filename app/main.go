232ac151ee102da0cbf782dcad3d6c39e9ad55be
Hellen Dou <hdou@squareup.com>
2022-02-08 16:40:41 -0800

    add circle stuff

--- app/main.go
+++ app/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/hdou731/wyre-go/circle"
+	"github.com/hdou731/wyre-go/tables"
 	"github.com/hdou731/wyre-go/wyre"
+	"github.com/jinzhu/gorm"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+// our initial migration function
+func initialMigration() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&tables.Transfer{})
+}
+
 func main() {
+	initialMigration()
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", testHandler)
-	r.HandleFunc("/getTransfer/{transferToken}", wyre.GetTransfer)
-	r.HandleFunc("/createOrder/{account}", wyre.CreateWalletOrderReservation)
-	r.HandleFunc("/executeOrder/{reservationId}", wyre.ExecuteWalletOrderReservation)
-	r.HandleFunc("/payout/{amount}", wyre.Payout)
-	r.HandleFunc("/getWallet/{walletToken}", wyre.GetWallet)
-	r.HandleFunc("/createWallet/{walletName}", wyre.CreateWallet)
+	r.HandleFunc("/wyre/getTransfer/{transferToken}", wyre.GetTransfer)
+	r.HandleFunc("/wyre/createOrder/{account}", wyre.CreateWalletOrderReservation)
+	r.HandleFunc("/wyre/executeOrder/{reservationId}", wyre.ExecuteWalletOrderReservation)
+	r.HandleFunc("/wyre/payout/{amount}", wyre.Payout)
+	r.HandleFunc("/wyre/getWallet/{walletToken}", wyre.GetWallet)
+	r.HandleFunc("/wyre/createWallet/{walletName}", wyre.CreateWallet)
+
+	r.HandleFunc("/circle/createBankAccount", circle.CreateBankAccount)
+	r.HandleFunc("/circle/createWirePayment/{trackingRef}", circle.CreateWirePayment)
+	r.HandleFunc("/circle/getBalance", circle.GetBalance)
+	r.HandleFunc("/circle/getWallets", circle.GetWallets)
 
 	http.ListenAndServe(":3000", r)
 }
